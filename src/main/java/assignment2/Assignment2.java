c525bb293a2be0d9386cdef1c0660e5a4d327c8c
nprpatkar <nprpatkar@gmail.com>
2019-03-09 18:56:34 +0530

    Init version of assigment2

--- src/main/java/assignment2/Assignment2.java
+++ src/main/java/assignment2/Assignment2.java
@@ -29,9 +29,35 @@ public class Assignment2 {
 
     void findOptimalProductionSequence(){
         Collections.sort(mobileList, new MobileComparator());
+        int totalManufactureTime = 0;
+        int assemblyTimeLeft = 0;
         for(Mobile m : mobileList){
             System.out.println(m.toString());
+            int waitTime = 0;
+            if(totalManufactureTime == 0){
+                //This is for the first mobile
+                totalManufactureTime += m.partManufactureTime;
+                assemblyTimeLeft += m.assemblyTime;
+                System.out.println("totalManufactureTime=" + totalManufactureTime + ", assemblyTimeLeft=" +
+                        assemblyTimeLeft + ", waitTime=" + waitTime);
+            } else {
+                if(assemblyTimeLeft < m.partManufactureTime){
+                    waitTime = m.partManufactureTime - assemblyTimeLeft;
+                    assemblyTimeLeft = 0;
+                } else {
+                    assemblyTimeLeft -= m.partManufactureTime;
+                }
+                totalManufactureTime += m.partManufactureTime;
+                int assemblyTimeLeftBefore = assemblyTimeLeft;
+                assemblyTimeLeft += m.assemblyTime;
+                System.out.println("totalManufactureTime=" + totalManufactureTime
+                        + ", assemblyTimeLeftBefore=" + assemblyTimeLeftBefore
+                        + ", assemblyTimeLeft=" + assemblyTimeLeft + ", waitTime=" + waitTime);
+            }
         }
+        totalManufactureTime += assemblyTimeLeft;
+
+        System.out.println("Production Time=" + totalManufactureTime);
     }
 
     public static void main(String[] args) throws IOException{
